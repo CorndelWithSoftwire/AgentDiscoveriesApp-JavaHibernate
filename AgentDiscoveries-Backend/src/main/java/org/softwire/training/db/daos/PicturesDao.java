a350fc8ce29dc9f7e5839ba9f9e0b763d28aedaf
Kenneth Siu <kenneth.siu@softwire.com>
2019-09-11 12:34:24 +0100

    Switch profile pictures to JPA

--- AgentDiscoveries-Backend/src/main/java/org/softwire/training/db/daos/PicturesDao.java
+++ AgentDiscoveries-Backend/src/main/java/org/softwire/training/db/daos/PicturesDao.java
@@ -1,46 +1,51 @@
 package org.softwire.training.db.daos;
 
-import org.jdbi.v3.core.Handle;
-import org.jdbi.v3.core.Jdbi;
 import org.softwire.training.models.ProfilePicture;
 
 import javax.inject.Inject;
-import java.sql.Blob;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import java.util.Optional;
 
 public class PicturesDao {
 
-    @Inject
-    Jdbi jdbi;
+    @Inject EntityManagerFactory entityManagerFactory;
 
     public Optional<ProfilePicture> getPicture(int userId) {
-        try (Handle handle = jdbi.open()) {
-            return handle.createQuery("SELECT * FROM profile_pictures WHERE user_id = :userId")
-                    .bind("userId", userId)
-                    .map((rs, ctx) -> new ProfilePicture(rs.getBytes("image"), rs.getString("content_type"), rs.getInt("user_id")))
-                    .findFirst();
-        }
+        EntityManager em = entityManagerFactory.createEntityManager();
+        em.getTransaction().begin();
+
+        ProfilePicture picture = em.find(ProfilePicture.class, userId);
+
+        em.getTransaction().commit();
+        em.close();
+
+        return Optional.ofNullable(picture);
     }
 
 
-    public void createOrUpdateUserPicture(int userId, Blob blob, String fileType) {
-        try (Handle handle = jdbi.open()) {
-            handle.createUpdate("INSERT INTO profile_pictures (image, content_type, user_id) "+
-                    "VALUES(:image , :content_type , :user_id) "+
-                    "ON DUPLICATE KEY UPDATE image = :image , content_type = :content_type")
-                    .bind("user_id", userId)
-                    .bind("image", blob)
-                    .bind("content_type", fileType)
-                    .execute();
-        }
+    public void createOrUpdateUserPicture(int userId, byte[] pictureBytes, String fileType) {
+        ProfilePicture picture = new ProfilePicture(pictureBytes, fileType, userId);
+
+        EntityManager em = entityManagerFactory.createEntityManager();
+        em.getTransaction().begin();
+
+        em.merge(picture);
+
+        em.getTransaction().commit();
+        em.close();
     }
 
     public void deleteUserPicture(int userId){
-        try (Handle handle = jdbi.open()) {
-            handle.createUpdate("DELETE FROM profile_picture "+
-                    "WHERE user_id = :user_id ")
-                    .bind("user_id", userId)
-                    .execute();
+        EntityManager em = entityManagerFactory.createEntityManager();
+        em.getTransaction().begin();
+
+        ProfilePicture picture = em.find(ProfilePicture.class, userId);
+        if (picture != null) {
+            em.remove(picture);
         }
+
+        em.getTransaction().commit();
+        em.close();
     }
 }
